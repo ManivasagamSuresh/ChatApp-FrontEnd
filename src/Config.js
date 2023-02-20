faae4b6b5470d877d66f88e8103d7f0b30300f97
ManivasagamSuresh <s.kishore123.64@gmail.com>
2023-02-20 15:38:48 +0530

    chat modified

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
-export const Config = {
-    api : "https://chatapp-cu6k.onrender.com"
-}
 // export const Config = {
-//     api : "http://localhost:5000"
+//     api : "https://chatapp-cu6k.onrender.com"
 // }
+export const Config = {
+    api : "http://localhost:5000"
+}
